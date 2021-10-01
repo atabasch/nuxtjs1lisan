df0a8081fb5f33ee2693f95f7c78c6807ae3320c
Furkan A <asw13tr@gmail.com>
2021-10-02 01:22:45 +0200

    request istekleri post olarak değiştirildi. RETURN EDLD

--- api/routers-words.js
+++ api/routers-words.js
@@ -13,13 +13,13 @@ let sqlWordsAll = `SELECT asw_words.*, asw_taxonomies.tax_name FROM asw_words
                     ORDER BY asw_words.word_id ASC`;
 
 router.post("/", (request, response)=>{
-  db.query(sqlWordsAll, (err, res, fields)=>{  response.status(200).json({ words:res }); });
+  db.query(sqlWordsAll, (err, res, fields)=>{  return response.status(200).json({ words:res }); });
 });
 
 
 router.post("/count", (request, response)=>{
   db.query("SELECT count(*) as total FROM asw_words", (err, res, fields) => {
-    response.status(200).json({count:res[0].total})
+    return response.status(200).json({count:res[0].total})
   })
 });
 
@@ -28,8 +28,8 @@ router.post("/count", (request, response)=>{
 router.post('/packets', (request, response)=>{
   let sql = `SELECT package_id as tax_id, package_name as tax_name, package_cover as tax_cover, package_datas as tax_datas, JSON_LENGTH(package_items) AS word_count FROM asw_packets WHERE package_status=1 ORDER BY package_id DESC`;
   db.query(sql, (err, res, fields) => {
-    if(err){ response.status(200).json( {err} ) }
-    response.status(200).json({items:res})
+    if(err){ return response.status(200).json( {err} ) }
+    return response.status(200).json({items:res})
   });
 });
 
@@ -39,15 +39,15 @@ router.post('/:package_id/package', (request, response)=>{
 
   let sqlPackage = `SELECT package_items, package_id as tax_id, package_name as tax_name, package_cover as tax_cover, package_datas as tax_datas, JSON_LENGTH(package_items) AS word_count FROM asw_packets WHERE package_status=1 AND package_id=${request.params.package_id} LIMIT 1`;
   db.query(sqlPackage, (err, resultPackage, fields)=>{
-    if(err){      response.status(404).json( {err} );     }else{
+    if(err){      return response.status(404).json( {err} );     }else{
       let itemIds = JSON.parse(resultPackage[0].package_items).join(',');
 
       let sqlWord = `SELECT *, ${selected} FROM asw_words WHERE word_status=1 AND word_id IN (${itemIds}) ORDER BY rand() DESC LIMIT 20`;
 
       db.query(sqlWord, (err, result, fields) => {
-        if(err){      response.status(404).json( {err} );     }else{
+        if(err){      return response.status(404).json( {err} );     }else{
 
-          response.status(200).json( {package:resultPackage[0], words:result} );
+          return response.status(200).json( {package:resultPackage[0], words:result} );
         } //if(err)
       });
 
@@ -63,7 +63,7 @@ router.post('/categories', (request, response)=>{
     (SELECT COUNT(*) FROM asw_words WHERE word_categories LIKE CONCAT('%"', tax_id, '"%')) AS word_count
     FROM asw_taxonomies WHERE tax_type='word_category' AND tax_status=1 HAVING word_count>3 ORDER BY tax_name ASC`;
   db.query(sql, (err, res, fields) => {
-    response.status(200).json({items:res})
+    return response.status(200).json({items:res})
   });
 });
 
@@ -74,12 +74,12 @@ router.post('/:category_id/category', (request, response)=>{
   let sqlWord = `SELECT *, ${selected} FROM asw_words WHERE word_categories LIKE \'%"${request.params.category_id}"%\' AND word_status>0 ORDER BY rand()`;
 
   db.query(sqlCategory, (err, res, fields)=>{
-    if(err){      response.status(404).json( {err} );     }else{
+    if(err){      return response.status(404).json( {err} );     }else{
       let category = res;
       db.query(sqlWord, (err, result, fields) => {
-        if(err){      response.status(404).json( {err} );     }else{
+        if(err){      return response.status(404).json( {err} );     }else{
 
-          response.status(200).json( {category:category[0], words:result} );
+          return response.status(200).json( {category:category[0], words:result} );
         } //if(err)
       });
 
@@ -96,7 +96,7 @@ router.post('/types', (request, response)=>{
     (SELECT COUNT(*) FROM asw_words WHERE word_status=1 AND word_type=tax_id) AS word_count
     FROM asw_taxonomies WHERE tax_type='word_type' AND tax_status=1 HAVING word_count>3 ORDER BY tax_name ASC`;
   db.query(sql, (err, res, fields)=>{
-    response.status(200).json({items:res});
+    return response.status(200).json({items:res});
   });
 });
 
@@ -108,12 +108,12 @@ router.post('/:type_id/type', (request, response)=>{
   let sqlWord = `SELECT *, ${selected} FROM asw_words WHERE word_status=1 AND word_type=${request.params.type_id} ORDER BY rand() DESC`;
 
   db.query(sqlType, (err, res, fields)=>{
-    if(err){      response.status(404).json( {err} );     }else{
+    if(err){      return response.status(404).json( {err} );     }else{
       let type = res;
       db.query(sqlWord, (err, result, fields) => {
-        if(err){      response.status(404).json( {err} );     }else{
+        if(err){      return response.status(404).json( {err} );     }else{
 
-          response.status(200).json( {type:type[0], words:result} );
+          return response.status(200).json( {type:type[0], words:result} );
         } //if(err)
       });
 
@@ -130,7 +130,7 @@ router.post('/:start/:limit', (request, response)=>{
   let sql = `SELECT asw_words.*, ${selected}, asw_taxonomies.tax_name FROM asw_words
                     INNER JOIN asw_taxonomies ON asw_taxonomies.tax_id=asw_words.word_type
                     ORDER BY asw_words.word_id ASC LIMIT ${start}, ${limit}`
-  db.query(sql, (err, res, fields)=>{response.status(200).json({ words:res }); });
+  db.query(sql, (err, res, fields)=>{return response.status(200).json({ words:res }); });
 });
 
 
