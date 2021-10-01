59387382f1dc7f5899be493653943761a344e202
Furkan A <asw13tr@gmail.com>
2021-10-01 14:15:53 +0200

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: '1Lisan',
+      name: 'nuxt1lisan',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'  
+      args: 'start'
     }
   ]
 }
