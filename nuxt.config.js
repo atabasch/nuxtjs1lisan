cccf72d4035d04e6676b774c38aab11fff37f19f
Furkan A <asw13tr@gmail.com>
2021-10-01 16:52:29 +0200

    env

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: (process.env.URL || "http://127.0.0.1:5001")+'/api'
+    baseURL: (process.env.URL || "127.0.0.1:5001")+'/api',
+    https: (process.env.HTTPS || false),
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
