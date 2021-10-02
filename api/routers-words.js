9c1d294a88bdacb7d45747df84a9cdd573c6888a
Furkan A <asw13tr@gmail.com>
2021-10-02 18:42:32 +0200

    routes düzenlemeleri

--- api/routers-words.js
+++ api/routers-words.js
@@ -32,7 +32,7 @@ router.post('/packets', (request, response)=>{
   let sql = `SELECT package_id as tax_id, package_name as tax_name, package_cover as tax_cover, package_datas as tax_datas, JSON_LENGTH(package_items) AS word_count FROM asw_packets WHERE package_status=1 ORDER BY package_id DESC`;
   db.query(sql, (err, res, fields) => {
     console.log("==============DB============")
-    console.log(res)
+    console.log(err)
     if(err){ return response.status(200).json( {err} ) }
     return response.status(200).json({items:res})
   });
