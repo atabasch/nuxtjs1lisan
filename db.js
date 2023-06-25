ee4f3696460f7e455369bf90fdc407f4f19320de
Furkan A <asw13tr@gmail.com>
2023-06-25 21:57:54 +0200

    db

--- db.js
+++ db.js
@@ -1,13 +1,30 @@
-var mysql = require('mysql');
-var connection  = mysql.createConnection({
+const mysql = require('mysql2');
+// var connection  = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: `${process.env.DB_USER}`,
+//   password: `${process.env.DB_PASSWORD}`,
+//   database: `${process.env.DB_NAME}`,
+//   port: process.env.DB_PORT || 3306,
+//   // multipleStatements: true
+// });
+
+var connection  = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
-  multipleStatements: true
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+  // multipleStatements: true
 });
 
-console.log(connection)
-
 module.exports = connection;
+
+
