9652e61ebde6f34d1d552dcce3c3dda32390fde2
Furkan A <asw13tr@gmail.com>
2023-08-25 12:40:50 +0000

    database sorunu düzeltildi

--- db.js
+++ db.js
@@ -9,9 +9,9 @@ const mysql = require('mysql2');
 // });
 
 var connection  = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
 
